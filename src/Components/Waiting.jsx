98070a0ee2d384b739ebe112b52461b2110474fb
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-03-24 16:34:00 +0300

    Added waiting for game to start page;

---
+++ src/Components/Waiting.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/App.css';
+import '../Styles/Waiting.css';
+
+export default function Waiting(props) {
+  const gameID = props.gameID;
+  const pieceTypeO = props.pieceTypeO;
+  const navigate = useNavigate();
+  const [imageSrc, setImageSrc] = useState();
+
+  // When starting the page, check if there is a gameID, if not, redirect to home page
+  useEffect(() => {
+    if (!gameID) {
+      navigate('/');
+    }
+  }, [gameID, navigate]);
+
+  // When the piece type changes, update the piece type in a temp varibale
+  useEffect(() => {
+    if (pieceTypeO) {
+      setImageSrc(require('../assets/blueO.png'));
+    }
+    else {
+      setImageSrc(require('../assets/redX.png'));
+    }
+  }, [setImageSrc]);
+
+
+  function copyGameID() {
+    navigator.clipboard.writeText(gameID);
+  }
+
+
+  return (
+    <div className='main-div'>
+      <div className="main-subdiv">
+        <div className='top'>
+          <h1>Waiting for a second player to join your game...</h1>
+          <hr style={{ width: "50%" }} />
+          <strong>{gameID}</strong>
+          <button onClick={copyGameID}>COPY GAME ID</button>
+        </div>
+
+
+        <div className='bottom'>
+          <h2>You will be playing as:</h2>
+          <img src={imageSrc} alt={pieceTypeO ? "BlueO" : "RedX"} />
+        </div>
+        <button onClick={() => navigate('/')}>BACK</button>
+      </div>
+    </div >
+  )
+}
