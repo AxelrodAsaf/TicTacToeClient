e6560093fb4b0ab9648d86ea0824f7a49316f753
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-03-25 19:19:32 +0300

    Added Win/Draw screens;

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import Start from './Components/Start';
 import HostGame from './Components/Host';
 import JoinGame from './Components/Join';
-import ViewGame from './Components/View';
+// import ViewGame from './Components/View';
 import Waiting from './Components/Waiting';
 import Connecting from './Components/Connecting';
 import Gameboard from './Components/Gameboard';
+import Xwin from './Components/Xwin';
+import Owin from './Components/Owin';
+import Draw from './Components/Draw';
 import { Routes, Route } from 'react-router-dom';
 import './Styles/App.css';
 
@@ -22,7 +25,10 @@ function App() {
         <Route path="/waiting" element={<Waiting username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} />
         <Route path="/joingame" element={<JoinGame username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} />
         <Route path="/connecting" element={<Connecting  username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} />
-        <Route path="/viewgame" element={<ViewGame  username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} />
+        <Route path="/Xwin" element={<Xwin/>} />
+        <Route path="/Owin" element={<Owin/>} />
+        <Route path="/Draw" element={<Draw/>} />
+        {/* <Route path="/viewgame" element={<ViewGame  username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} /> */}
         <Route path="/gameboard/:gameID" element={<Gameboard  username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} />
         <Route path="*" element={<Start username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} />} />
       </Routes>
---
+++ src/Components/Owin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Owin() {
+  return (
+    <div>O WINS!</div>
+  )
+}
