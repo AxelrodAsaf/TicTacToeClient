cfbc4768792651bb1cbb381ff49dc7a7977a154e
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-03-25 10:23:55 +0300

    Clean of waiting page;

--- src/Components/Waiting.jsx
+++ src/Components/Waiting.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Styles/App.css';
@@ -8,6 +9,8 @@ export default function Waiting(props) {
   const pieceTypeO = props.pieceTypeO;
   const navigate = useNavigate();
   const [imageSrc, setImageSrc] = useState();
+  const [gameData, setGameData] = useState();
+  const [isGameFull, setIsGameFull] = useState(false);
 
   // When starting the page, check if there is a gameID, if not, redirect to home page
   useEffect(() => {
@@ -16,6 +19,7 @@ export default function Waiting(props) {
     }
   }, [gameID, navigate]);
 
+  // Set the image source for the player's game piece
   useEffect(() => {
     if (pieceTypeO) {
       setImageSrc(require('../assets/blueO.png'));
@@ -25,7 +29,39 @@ export default function Waiting(props) {
     }
   }, [setImageSrc, pieceTypeO]);
 
+  // Check if there are 2 players in the game
+  useEffect(() => {
+    async function check() {
+      // Find the game with the given gameID
+      async function findGame() {
+        const response = await axios.get(`http://localhost:8000/getgame/${gameID}`);
+        console.log(response.data.players.length);
+        setGameData(response.data);
+        if (response.data.players.length === 2) {
+          setIsGameFull(true);
+        }
+      }
+      if (!isGameFull) {
+        await findGame();
+      }
+    }
+    check();
+  }, [gameID, gameData, isGameFull]);
 
+  // Navigate to gameboard if the game is full
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (!isGameFull) {
+        navigate(`/gameboard/${gameID}`);
+      } else {
+        navigate(`/gameboard/${gameID}`);
+        clearInterval(intervalId);
+      }
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, [isGameFull, gameID, navigate]);
+
+  // Copy the game ID to the clipboard
   function copyGameID() {
     navigator.clipboard.writeText(gameID);
   }
@@ -40,8 +76,6 @@ export default function Waiting(props) {
           <strong>{gameID}</strong>
           <button onClick={copyGameID}>COPY GAME ID</button>
         </div>
-
-
         <div className='bottom'>
           <h2>You will be playing as:</h2>
           <img src={imageSrc} alt={pieceTypeO ? "BlueO" : "RedX"} />
