0c41229a553f395b3167c2b8fb84b343f8a5ddea
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-03-27 03:13:54 +0300

    Cleaning; Deleted irrelevant files;

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import Start from './Components/Start';
 import HostGame from './Components/Host';
 import JoinGame from './Components/Join';
-// import ViewGame from './Components/View';
 import Waiting from './Components/Waiting';
-import Connecting from './Components/Connecting';
 import Gameboard from './Components/Gameboard';
 import Xwin from './Components/Xwin';
 import Owin from './Components/Owin';
@@ -29,17 +27,15 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={"/"} element={<Start username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
+        <Route path="*" element={<Start username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
+        <Route path="/" element={<Start username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
         <Route path="/hostgame" element={<HostGame username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
         <Route path="/waiting" element={<Waiting username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
         <Route path="/joingame" element={<JoinGame username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
-        <Route path="/connecting" element={<Connecting username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
+        <Route path="/gameboard/:gameID" element={<Gameboard username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
         <Route path="/Xwin" element={<Xwin />} />
         <Route path="/Owin" element={<Owin />} />
         <Route path="/Draw" element={<Draw />} />
-        {/* <Route path="/viewgame" element={<ViewGame  username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket}/>} /> */}
-        <Route path="/gameboard/:gameID" element={<Gameboard username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
-        <Route path="*" element={<Start username={username} setUsername={setUsername} pieceTypeO={pieceTypeO} setPieceTypeO={setPieceTypeO} gameID={gameID} setGameID={setGameID} socket={socket} />} />
       </Routes>
     </div>
   );
